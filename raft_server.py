5ee1eac1cfb5573a8de0cc949728a615f27b4d0a
Ken Reed <github@kmreed.net>
2020-03-24 11:29:07 -0700

    Checkpoint 20200324.1129

--- raft_server.py
+++ raft_server.py
@@ -5,8 +5,19 @@ Will implement the Raft protocol.
 from xmlrpc.server import SimpleXMLRPCServer
 #from xmlrpc.server import SimpleXMLRPCServerHandler
 
+class LogEntry():
+    entry = ""
+    def SetValue(self, value):
+        self.value = value
+
 class RaftNode():
     """docstring for RaftNode."""
+
+    '''These will need to be persisted'''
+    currentTerm = 0
+    votedFor = 0
+    log = []
+
 
     def __init__(self, arg):
         # super(RaftNode, self).__init__()
@@ -14,7 +25,16 @@ class RaftNode():
         self.server = SimpleXMLRPCServer(('localhost', 8000))
         self.server.register_introspection_functions()
 
-        self.server.register_function(pow)
+        def RequestVote(term, candidateId, lastLogIndex, lastLogTerm):
+            print("RequestVote")
+            return {currentTerm, true}
+        self.server.register_function(RequestVote)
+
+        def AppendEntries(term, leaderId, prevLogIndex, prevLogTerm,
+                            entries, leaderCommit):
+            print("AppendEntries")
+            return {currentTerm, true}
+        self.server.register_function(AppendEntries)
 
         def adder_function(x, y):
             return x+y
--- test_client.py
+++ test_client.py
@@ -1,7 +1,6 @@
 import xmlrpc.client
 
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
-print(s.pow(2, 2))
 print(s.add(5, 6))
 
 print(s.system.listMethods())
