0c3f9cd32cecca8dbe282ddb161c1980f61777e4
Ken Reed <github@kmreed.net>
2020-03-27 18:12:56 -0700

    Checkpoint 20200327.1812
    A little more code in test_log.py.

--- test_log.py
+++ test_log.py
@@ -3,15 +3,16 @@ from log import Log
 def main():
     log = Log(0)
 
-    (status, entry) = log.get_entry(1)
-    print("status: " + str(status))
-    status = log.add_entry(1, 1, "Test Entry")
-    print("satus: " + str(status))
-    (status, entry) = log.get_entry(1)
-    if status:
-        print("entry: " + entry)
-    else:
-        print("entry for slot 1 not found")
+    for i in range(10):
+        (status, entry) = log.get_entry(i)
+        print("status: " + str(status))
+        status = log.add_entry(1, i, "Test Entry")
+        print("satus: " + str(status))
+        (status, entry) = log.get_entry(i)
+        if status:
+            print("entry: " + entry)
+        else:
+            print(F'entry for slot {i} not found')
 
 
 if __name__ == "__main__":
