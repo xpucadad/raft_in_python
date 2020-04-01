81ce19baf7e879dad2e0b66de99b9b8f196873bb
Ken Reed <github@kmreed.net>
2020-04-01 11:53:26 -0700

    Checkpoint 2020040.1152
    Not working - need to figure out how to integrate the RPC calls to the classes. Probably have to put that at main level and somehow provide them with a class instance.

--- raft_server.py
+++ raft_server.py
@@ -8,67 +8,67 @@ from server_states import *
 
 class RaftServer():
     """docstring for RaftServer."""
-    serverState = FOLLOWER
+    server_state = FOLLOWER
 
     '''These will need to be persisted'''
     '''Put them into a State class which can handle the persistence'''
-    currentTerm = 0
-    votedFor = 0
-    serverId = 1
-    nextIndex = 1
+    current_term = 0
+    voted_for = 0
+    server_id = 1
+    next_index = 1
 
-    commitIndex = 0
-    lastApplied = 0
+    commit_index = 0
+    last_applied = 0
 
     server = SimpleXMLRPCServer(('localhost', 8000))
     server.register_introspection_functions()
 
-    def __init__(self, arg):
+    def __init__(self, server_id):
         # super(RaftNode, self).__init__()
-        self.arg = arg
+        self.server_id = server_id
 
-    def RequestVote(term, candidateId, lastLogIndex, lastLogTerm):
+    def RequestVote(term, candidate_id, last_log_index, last_log_term):
         print("RequestVote")
-        return {currentTerm, true}
+        return {self.current_term, true}
 
     server.register_function(RequestVote)
 
-    def AppendEntries(term, leaderId, prevLogIndex, prevLogTerm,
-                        entries, leaderCommit):
+    def AppendEntries(term, leader_id, prev_log_index, prev_log_term,
+                        entries, leader_commit):
         # ignore requests from past terms
-        if term < currentTerm:
-            return (currentTerm, False)
+        if term < self.current_term:
+            return (self.current_term, False)
 
-        if prevLogIndex != 0:
-            # get log entry at prevLogIndex
-            (status, eTerm, _) = log.get_entry(prevLogIndex)
+        if prev_log_index != 0:
+            # get log entry at prev_log_index
+            (status, e_term, _) = log.get_entry(prev_log_index)
             if not status:
-                return(currentTerm, False)
+                return(self.current_term, False)
 
-            if eTerm != prevLogTerm:
-                return(currentTerm, False)
+            if e_term != prev_log_term:
+                return(self.current_term, False)
 
-            newIndex = prevLogIndex
+            new_index = prev_log_index
 
-            (status, eTerm, _) = log.get_entry(newIndex)
-            if status and (term != eTerm):
+            (status, e_term, _) = log.get_entry(new_index)
+            if status and (term != e_term):
                 # delete entry and all following
-                None
+                Pass
             # append entries
             else:
             # don't know yet
-                None
+                Pass
 
-        currentTerm = term
+        self.current_term = term
 
         print("AppendEntries")
         print('term: ' + str(term))
-        print('leaderId: ' + str(leaderId))
-        print('prevLogIndex: ' + str(prevLogIndex))
-        print('prevLogTerm: ' + str(prevLogTerm))
+        print('leader_id: ' + str(leader_id))
+        print('prev_log_index: ' + str(prev_log_index))
+        print('prev_log_term: ' + str(prev_log_term))
         print('entries: ' + str(entries))
-        print('leaderCommit: ' + str(leaderCommit))
-        return (currentTerm, True)
+        print('leaderCommit: ' + str(leader_commit))
+        return (self.current_term, True)
 
     server.register_function(AppendEntries)
 
@@ -84,6 +84,6 @@ class RaftServer():
         print("RaftNode: Hello " + self.arg)
 
 if __name__ == '__main__':
-    raftServer = RaftServer("Ken")
-    raftServer.serve_forever()
+    raft_server = RaftServer("Ken")
+    raft_server.serve_forever()
     print("__main__: Goodbye World!")
