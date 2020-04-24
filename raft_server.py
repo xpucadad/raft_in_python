9d235f2e19fe9748300be47abb34692c7fe63475
Ken Reed <github@kmreed.net>
2020-04-24 12:28:41 -0700

    20200424.1228 Intermediate CP
    The patient is open.

--- raft_server.py
+++ raft_server.py
@@ -25,26 +25,31 @@ MAX_TIMEOUT = 2
     self.request_queues[self.server_id] queue
 '''
 class RaftNode():
-    def __init__(self, server_count, server_id, connection):
+    def __init__(self, server_count, server_id, pipes):
         print('RaftNode init', server_count, server_id)
         self.server_count = server_count
         self.server_id = server_id
-        self.connection = connection
+        self.pipes = pipes
+        self.connection = pipes[server_id].server_side
 
         # retrieve persistent state
         self.persisted_state = PersistedState(server_id)
         current_state = self.persisted_state.get_state()
-        self.current_term = current_state['current_term']
-        self.voted_for = current_state['voted_for']
+        # self.current_term = current_state['current_term']
+        # self.voted_for = current_state['voted_for']
 
         # initialize non-persistent state
         self.role = FOLLOWER
+        self.last_log_index = 0
+        self.last_log_term = 0
         self.commit_index = 0
         self.last_applied = 0
         self.next_index = []
         self.match_index = []
         self.stopped = False
         self.leader = None
+
+        self.server_methods = ServerMethods(self, self.persisted_state)
 
  
     def run(self):
@@ -59,6 +64,8 @@ class RaftNode():
 
             if not have_request:
                 print('server %d timed out waiting for a request' % self.server_id )
+                if self.role == FOLLOWER:
+                    self._run_for_leader()
                 continue
 
             request = self.connection.recv()
@@ -84,24 +91,73 @@ class RaftNode():
                 print(error)
                 response['status'] = False
                 response['error'] = error
-                continue
- 
-            if operation == 'stop':
-                response['status'] = self.stop()
- 
             else:
-                response['error'] = 'unrecognized node call %s; ingoring' % operation
-                response['status'] = False
- 
+                method_return = self.server_methods.run_method(operation, request)
+                response['status'] = method_return['status']
+                response['method_return'] = method_return
+
             print('server %d about to return response ' 
                         % self.server_id, response)
             self.connection.send(response)
 
         print('server %d stopped running' % self.server_id)
 
+    def _run_for_leader(self):
+        self.role = CANDIDATE
+        new_term = self.persisted_state.get_current_term() + 1
+        self.persisted_state.set_current_term = new_term
+ 
+        request = {
+            'from': self.server_id,
+            'operation': 'request_vote'
+            'term': new_term,
+            'candiidate_id': self.server_id,
+            'last_log_index': self.last_log_index,
+            'last_log_term': self.last_log_term
+        }
+
+        statuses = [None] * self.server_count
+        except_list = [self.server_id]
+        statuses = broadcast_request(self.pipes, request, except_list)
+        statuses[self.server_id] = True
+
+ 
     def stop(self):
         self.stopped = True
         return True
+
+class ServerMethods():
+    def __init__(self, my_server, persistent_state):
+        self.my_server = my_server
+        self.persistent_state = persistent_state
+        self.registered_methods = {}
+        self.registered_methods.update({'stop': self.stop})
+ 
+    def run_method(self, operation, args):
+        method = self.registered_methods[operation]
+        result = {}
+        if not method:
+            result['status'] = False
+            result['error'] = 'No method for operation %s' % operation
+        else:
+            method_return = method(args)
+            result['status'] = method_return['status']
+            result['returned'] = method_return
+
+        return result
+
+
+    def stop(self, args):
+        status = self.my_server.stop()
+        return {'status': status}
+
+    def request_vote(self, args):
+        return {'status': True}
+
+
+    def append_entries(self, args):
+        return {'status': True}
+
 
 '''
     This class processes XMLRPC calls sent to 
@@ -144,6 +200,36 @@ class RaftClient(SimpleXMLRPCServer):
         print('client shutting down')
         self.shuttingdown = True
         return True
+
+class ClientMethods():
+    def __init__(self, client, server_count, client_id, pipes):
+        
+        print('Client Methods %d' % client_id)
+        self.client = client    # This is the instance of RaftClient that
+                                # created this instance of ClientMethods
+        self.server_count = server_count
+        self.client_id = client_id
+        self.pipes = pipes
+        self.leader = 0     # assume leader is 0
+
+    
+    def shutdown(self):
+        print('client requested a stop')
+        request = {'type': 'request', 'operation': 'stop', 'from': self.client_id}
+        # Our server_id is the same as SERVER_COUNT so the follwing
+        # iteration will get every server but not this server.
+        # self.server_id == SERVER_COUNT == len(queues
+        # probably should pass in SERVER_COUNT explicitly in __init__.
+        statuses = broadcast_request(self.pipes, request, [])
+
+        print('ClientMethods results from shutdown request: ', statuses)
+        print('stop server proxy')
+        self.client.shutdown()
+
+        return True
+
+    def list_methods(self):
+        return ['shutdown']
 
 '''
 This thread sends a request to one server and then processes the 
@@ -197,36 +283,6 @@ def broadcast_request(pipes, request, except_list):
 
     return statuses
 
-class ClientMethods():
-    def __init__(self, client, server_count, client_id, pipes):
-        
-        print('Client Methods %d' % client_id)
-        self.client = client    # This is the instance of RaftClient that
-                                # created this instance of ClientMethods
-        self.server_count = server_count
-        self.client_id = client_id
-        self.pipes = pipes
-        self.leader = 0     # assume leader is 0
-
-    
-    def shutdown(self):
-        print('client requested a stop')
-        request = {'type': 'request', 'operation': 'stop', 'from': self.client_id}
-        # Our server_id is the same as SERVER_COUNT so the follwing
-        # iteration will get every server but not this server.
-        # self.server_id == SERVER_COUNT == len(queues
-        # probably should pass in SERVER_COUNT explicitly in __init__.
-        statuses = broadcast_request(self.pipes, request, [])
-
-        print('ClientMethods results from shutdown request: ', statuses)
-        print('stop server proxy')
-        self.client.shutdown()
-
-        return True
-
-    def list_methods(self):
-        return ['shutdown']
-
 ''' This is the top level entry point for all subprocesses '''
 def start_server(server_count, server_id, connection):
     print ('start_server', server_count, server_id)
@@ -239,59 +295,46 @@ def start_server(server_count, server_id, connection):
     except KeyboardInterrupt:
         print('server got keyboard interrupt; exitting')
 
-# class ServerMethods():
-    # def __init__(self, p_state):
-    #     self.p_state = p_state
-
-    # def request_vote(request):
-    #     # term, candidate_id, last_log_index, last_log_term):
-    #     print('Vote Requested', request)
-    #     state = p_state.get_state()
-    #     return {state['current_term'], True}
-    #     return True
-
-    # def list_methods(self):
-    #     return ['request_vote']
 
 
-def AppendEntries(term, leader_id, prev_log_index, prev_log_term,
-                    entries, leader_commit):
-    print("AppendEntries")
-    print('term: ' + str(term))
-    print('leader_id: ' + str(leader_id))
-    print('prev_log_index: ' + str(prev_log_index))
-    print('prev_log_term: ' + str(prev_log_term))
-    print('entries: ' + str(entries))
-    print('leaderCommit: ' + str(leader_commit))
+# def AppendEntries(term, leader_id, prev_log_index, prev_log_term,
+#                     entries, leader_commit):
+#     print("AppendEntries")
+#     print('term: ' + str(term))
+#     print('leader_id: ' + str(leader_id))
+#     print('prev_log_index: ' + str(prev_log_index))
+#     print('prev_log_term: ' + str(prev_log_term))
+#     print('entries: ' + str(entries))
+#     print('leaderCommit: ' + str(leader_commit))
 
-    p_state = persisted_state.get_state()
-    current_term = p_state['current_term']
-    print('current_term: ' + str(current_term))
-    print('voted_for: ' + str(p_state['voted_for']))
+#     p_state = persisted_state.get_state()
+#     current_term = p_state['current_term']
+#     print('current_term: ' + str(current_term))
+#     print('voted_for: ' + str(p_state['voted_for']))
 
-    if term < current_term:
-        return (current_term, False)
+#     if term < current_term:
+#         return (current_term, False)
 
-    (status, entry_term, _) = log.get_entry(prev_log_index)
-    if not status:
-        return(current_term, False)
+#     (status, entry_term, _) = log.get_entry(prev_log_index)
+#     if not status:
+#         return(current_term, False)
 
-    if entry_term != prev_log_term:
-        return(current_term, False)
+#     if entry_term != prev_log_term:
+#         return(current_term, False)
 
-    (status, entry_term, _) = log.get_entry(prev_log_index)
-    if status and (term != entry_term):
-        # delete entry and all following
-        Pass
-    # append entries
-    else:
-    # don't know yet
-        Pass
+#     (status, entry_term, _) = log.get_entry(prev_log_index)
+#     if status and (term != entry_term):
+#         # delete entry and all following
+#         Pass
+#     # append entries
+#     else:
+#     # don't know yet
+#         Pass
 
-    p_state['current_term'] = term
-    persisted_state.set_state(p_state)
+#     p_state['current_term'] = term
+#     persisted_state.set_state(p_state)
 
-    return (term, True)
+#     return (term, True)
 
 class RaftPipe():
     def __init__(self):
